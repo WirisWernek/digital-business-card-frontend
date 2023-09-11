f60b6b3da79859cc044310192eb1a0f6779ca4c1
Wiris Wernek <wiris.wernek@itixti.com.br>
2023-09-10 21:28:43 -0300

    feat: add components da pagina

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: 'home',
+		title: 'Wiris Wernek',
+		loadChildren: () => import('./cartao-de-visitas/cartao-de-visitas.module').then(m => m.CartaoDeVisitasModule)
+	},	
+	{ 	path: '**', 
+		redirectTo: "home" 
+	}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
