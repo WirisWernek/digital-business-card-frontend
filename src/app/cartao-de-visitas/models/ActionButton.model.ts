8dde4d3714ffdb2e18b8c97a894f09fb22e22fd3
Wiris Wernek <rafaeljwerneck43@gmail.com>
2023-09-11 22:45:27 -0300

    feat: add services para gerenciamento dos botões

---
+++ src/app/cartao-de-visitas/models/ActionButton.model.ts
@@ -0,0 +1,25 @@
+export class ActionButtonModel {
+	link: string;
+	imagem: string;
+	descricao: string;
+	idStyle: string;
+	styles: Array<string>;
+  
+	constructor() {
+	  this.link = '';
+	  this.imagem = '';
+	  this.descricao = '';
+	  this.idStyle = '';
+	  this.styles = Array<string>();
+	}
+  
+	build(link: string, imagem: string, descricao: string, idStyle: string, styles: Array<string>) {
+	  this.link = link;
+	  this.imagem = imagem;
+	  this.descricao = descricao;
+	  this.idStyle = idStyle;
+	  this.styles = styles;
+	  return this;
+	}
+  }
+  
