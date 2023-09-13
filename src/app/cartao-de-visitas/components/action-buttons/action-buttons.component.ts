cea1d749ebee92105aa94aced3fb4146a266e290
Wiris Wernek <wiris.wernek@itixti.com.br>
2023-09-12 22:51:19 -0300

    feat: add links dos actions buttons

--- src/app/cartao-de-visitas/components/action-buttons/action-buttons.component.ts
+++ src/app/cartao-de-visitas/components/action-buttons/action-buttons.component.ts
@@ -1,20 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { ActionButtonModel } from '../../models/ActionButton.model';
+import { ContactLinkModel } from '../../models/ContactLink.model';
 import { ActionButtonsService } from '../../services/action-buttons.service';
+import { ContactLinksService } from '../../services/contact-links.service';
 
 @Component({
   selector: 'app-action-buttons',
   templateUrl: './action-buttons.component.html',
-  styleUrls: ['./action-buttons.component.scss']
+  styleUrls: ['./action-buttons.component.scss'],
 })
 export class ActionButtonsComponent implements OnInit {
+  actionsButtons: Array<ActionButtonModel> = [];
+  contactLinks: ContactLinkModel = new ContactLinkModel();
 
-	actionsButtons: Array<ActionButtonModel> = [];
+  constructor(
+    private actionButtonService: ActionButtonsService,
+    private contactLinkService: ContactLinksService
+  ) {}
 
-	constructor(private actionButtonService: ActionButtonsService){}
+  ngOnInit(): void {
+    this.actionsButtons = this.actionButtonService.getAllActionsButtons();
+    this.contactLinks = this.contactLinkService.getAllContactLinks();
+  }
 
-	ngOnInit(): void {
-		this.actionsButtons = this.actionButtonService.getAllActionsButtons();
-	}
-
+  changeContactLink(idStyle: string) {
+    switch (idStyle) {
+      case 'whatsapp':
+        return this.contactLinks.whatsapp;
+      case 'email':
+        return this.contactLinks.email;
+      case 'telefone':
+        return this.contactLinks.telefone;
+      case 'download':
+        return this.contactLinks.download;
+      case 'localizacao':
+        return this.contactLinks.localizacao;
+      default:
+        return '';
+    }
+  }
 }
