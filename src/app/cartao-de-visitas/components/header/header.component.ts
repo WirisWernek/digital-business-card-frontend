26d2eff0b6ec79e4ce36bcae3d7716b546a76d1f
Wiris Wernek <rafaeljwerneck43@gmail.com>
2023-09-13 21:07:33 -0300

    fix: teste correção blink

--- src/app/cartao-de-visitas/components/header/header.component.ts
+++ src/app/cartao-de-visitas/components/header/header.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  subtitle: string;
+  index: number = -1;
 
+  text: string = 'Backend Web Developer';
+  textArray: Array<string> = this.text.split('');
+
+  constructor() {
+    this.subtitle = '';
+  }
+  ngOnInit(): void {
+    this.loadSubtitleWithEffect();
+  }
+
+  loadSubtitleWithEffect() {
+    if (this.index < this.textArray.length - 1)
+      setTimeout(() => {
+        this.index += 1;
+        this.subtitle += this.textArray[this.index];
+        this.loadSubtitleWithEffect();
+      }, 150);
+  }
 }
