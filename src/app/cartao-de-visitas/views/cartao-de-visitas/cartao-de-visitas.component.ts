6355b1a185ccf7cf564c3d4f9e0bb6e87da2d28b
Wiris Wernek <wiris.wernek@itixti.com.br>
2023-09-15 23:29:07 -0300

    feat: melhora responsividade da aplicação

--- src/app/cartao-de-visitas/views/cartao-de-visitas/cartao-de-visitas.component.ts
+++ src/app/cartao-de-visitas/views/cartao-de-visitas/cartao-de-visitas.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-cartao-de-visitas',
   templateUrl: './cartao-de-visitas.component.html',
-  styleUrls: ['./cartao-de-visitas.component.scss']
+  styleUrls: ['./cartao-de-visitas.component.scss'],
 })
 export class CartaoDeVisitasComponent {
-
+  typeVisible: string = '1';
+  reciverResponse(response: string) {
+    console.log('Foi emitido e evento e chegou ao componente pai os dados >>>> {}', response);
+    this.typeVisible = response;
+  }
 }
