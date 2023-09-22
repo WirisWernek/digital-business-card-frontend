cd02c84107c3dca5a27152f2a909d0bc61367e54
Wiris Wernek <rafaeljwerneck43@gmail.com>
2023-09-21 23:37:23 -0300

    feat: add logica parcial dos botões

--- src/app/cartao-de-visitas/services/optional-buttons.service.ts
+++ src/app/cartao-de-visitas/services/optional-buttons.service.ts
@@ -2,18 +2,22 @@ import { Injectable } from '@angular/core';
 import { OptionalButtons } from '../models/OptionalButtons.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OptionalButtonsService {
-
-	optionalButtons: OptionalButtons;
+  optionalButtons: OptionalButtons;
 
   constructor() {
-	this.optionalButtons = new OptionalButtons();
-   }
+    this.optionalButtons = new OptionalButtons();
+  }
 
-  getAllOptionButtons(){
-	this.optionalButtons.build(true, "Por enquanto não tem e é sobre isso!", true, true);
-	return this.optionalButtons;
+  getAllOptionButtons() {
+    this.optionalButtons.build(
+      true,
+      'Por enquanto não tem e é sobre isso!',
+      true,
+      true
+    );
+    return this.optionalButtons;
   }
 }
