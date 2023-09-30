b3cb3d9640acdee02077799d380265f3aefb6367
Wiris Wernek <wiris.wernek@itixti.com.br>
2023-09-30 00:30:15 -0300

    fix: corige para novos links

--- src/app/cartao-de-visitas/services/list-projects-links.service.ts
+++ src/app/cartao-de-visitas/services/list-projects-links.service.ts
@@ -11,70 +11,56 @@ export class ListProjectsLinksService {
     const listLinks = new Array<LinkModel>();
     listLinks.push(
       new LinkModel().build(
-        'https://wiriswernek.vercel.app', 
+        'https://home-wiriswernek.vercel.app', 
         'Portfólio', 
         ['col-11','button']
       )
     );
     listLinks.push(
       new LinkModel().build(
-        'https://digital-card.wiriswernek.vercel.app',
+        'https://dcb-wiriswernek.vercel.app',
         'Cartão de Visitas Digital',
         ['col-11', 'button']
       )
     );
     listLinks.push(
       new LinkModel().build(
-        'https://previsao-tempo.wiriswernek.vercel.app',
+        'https://previsao-tempo-wiriswernek.vercel.app',
         'Previsão do Tempo',
         ['col-11', 'button']
       )
     );
     listLinks.push(
       new LinkModel().build(
-        'https://github-consumer.wiriswernek.vercel.app',
+        'https://github-consumer-wiriswernek.vercel.app',
         'GitHub Consumer',
         ['col-11', 'button']
       )
     );
     listLinks.push(
       new LinkModel().build(
-        'https://finance.wiriswernek.vercel.app',
+        'https://finance-wiriswernek.vercel.app',
         'Finance',
         ['col-11', 'button']
       )
     );
     listLinks.push(
       new LinkModel().build(
-        'https://gerador-de-senhas.wiriswernek.vercel.app',
+        'https://gerador-de-senhas-wiriswernek.vercel.app',
         'Gerador de Senhas',
         ['col-11', 'button']
       )
     );
     listLinks.push(
       new LinkModel().build(
-        'https://projeto-hotel-paraiso.wiriswernek.vercel.app',
-        'Projeto Hotel Paraíso',
-        ['col-11', 'button']
-      )
-    );
-    listLinks.push(
-      new LinkModel().build(
-        'https://projeto-anna-bella.wiriswernek.vercel.app',
-        'Projeto Anna Bella',
-        ['col-11', 'button']
-      )
-    );
-    listLinks.push(
-      new LinkModel().build(
-        'https://snake-game.wiriswernek.vercel.app',
+        'https://snake-game-wiriswernek.vercel.app',
         'Snake Game',
         ['col-11', 'button']
       )
     );
     listLinks.push(
       new LinkModel().build(
-        'https://jogo-da-velha.wiriswernek.vercel.app',
+        'https://jogo-da-velha-wiriswernek.vercel.app',
         'Jogo da Velha',
         ['col-11', 'button']
       )
