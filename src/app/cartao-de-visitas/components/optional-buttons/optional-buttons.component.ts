295b8fbe94bce6213e201a0d742710ba61829d78
Wiris Rafael Januario Wernek <rafaeljwerneck43@gmail.com>
2023-10-16 23:47:55 -0300

    Update optional-buttons.component.ts

--- src/app/cartao-de-visitas/components/optional-buttons/optional-buttons.component.ts
+++ src/app/cartao-de-visitas/components/optional-buttons/optional-buttons.component.ts
@@ -33,7 +33,7 @@ export class OptionalButtonsComponent implements OnInit {
 
   ngOnInit(): void {
     this.optionalButtons = this.optionalButtonsService.getAllOptionButtons();
-	this.compartilharEnable = window.navigator.canShare();
+	  this.compartilharEnable = window.navigator?.canShare() ? true : false;
   }
 
   copiarChavePix() {
