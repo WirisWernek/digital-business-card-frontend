15656aac960b97096287ff4a83c931888fa25a0a
Wiris Wernek <rafaeljwerneck43@gmail.com>
2023-10-26 21:44:42 -0300

    inicia branch de dados

--- src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  PRODUCTION: false,
-  API_URL: 'http://localhost:4000',
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  PRODUCTION: true,
-  API_URL: 'https://api-wiriswernek.vercel.app',
-};
